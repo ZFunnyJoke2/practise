f6c2527e48a30b3a7e0aa3199f39d144e7e599e8
ZFunnyJoke2 <zmonetov@gmail.com>
2024-05-27 20:11:21 +0700

    Initial commit

--- src/components/MainInformantion.vue
+++ src/components/MainInformantion.vue
@@ -5,8 +5,35 @@
 <template>
   <div class="parallax">
     <div class="mask">
-      <div class="container">
+      <div class="main">
+        <div class="first-block">
+          <div class="first-information">
+            <h2>Welcome to The Soprano</h2>
+            <div class="first-info">
+              <img src="../assets/img/MainSoprano.jpg" alt="SopranoMain">
+              <pre>
+                The Sopranos is a crime drama TV series created by David Chase, and
+                widely recognized as one of the greatest series of all time.
 
+                It follows protagonist Tony Soprano (James Gandolfini), a New Jersey
+                -based Italian-American mob boss, who struggles to manage his family and
+                criminal life. Suffering from panic attacks, Tony attends therapy sessions
+                with psychiatrist Jennifer Melfi (Lorraine Bracco), whom he confides in.
+
+                Throughout the six seasons of the series, Tony has to contend with his
+                uncle, Junior (Dominic Chianese); his wife, Carmela (Edie Falco); and
+                threats from mobsters both within the family and external, such as the
+                New York City-based Lupertazzi family.
+
+                A prequel film to The Sopranos has been confirmed. Titled The Many Saints
+                of Newark, it centers on the 1967
+                Newark riots and will feature a young Tony Soprano (Michael Gandolfini),
+                among other characters. The film was
+                released on September 24, 2021.
+              </pre>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -14,9 +41,9 @@
 
 <style>
 .parallax {
-  background-image: url(../assets/img/Background-image.jpg);
-  height: 2000px;
-  background-attachment: fixed;
+  background: url(../assets/img/Background-image.jpg) fixed no-repeat;
+  background-size: cover;
+  height: 100vh;
   display: flex;
   justify-content: center;
   color: #ffffff;
@@ -31,5 +58,34 @@
   background: rgba(0,0,0,.6);
   z-index: 2;
 }
+.main{
+  height: 100vh;
+  width: 90%;
+  margin-left: 5%;
+  background:rgba(0,0,0,0.5);
+}
+.first-info{
+  margin-top: 40px;
+  display: flex;
+}
 
+.first-information img{
+  width: 400px;
+  height: 600px;
+  object-fit: cover;
+  border: 5px solid black;
+  margin-left: 150px;
+}
+.first-information pre{
+  padding-top: 20px;
+  font-size: 18px;
+  word-break: break-all;
+  line-height: 30px;
+}
+.first-information h2{
+  padding-top: 50px;
+  display: flex;
+  justify-content: center;
+  color: indianred;
+}
 </style>
